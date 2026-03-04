c2fd2394030a4236c84f98e11cde3372cbb88caf
Nguyễn Huỳnh Tấn Đạt <jacknyu2@gmail.com>
2026-03-04 22:41:41 +0700

    add relief_items DTO
    add relief_items DTO

---
+++ API/Models/ReliefItem.cs
@@ -0,0 +1,12 @@
+namespace Flood_Rescue_Coordination.API.Models;
+
+public class ReliefItem
+{
+    public int ItemId { get; set; }
+    public string ItemCode { get; set; } = string.Empty;
+    public string ItemName { get; set; } = string.Empty;
+    public int CategoryId { get; set; }
+    public string Unit { get; set; } = string.Empty;
+    public bool IsActive { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
